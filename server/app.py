2416abf5d925dedaa5084c76e10263b11f8eae8e
Bill Brown <bill.a.brown90@gmail.com>
2023-04-26 16:28:46 -0700

    working with sql alchemy and flask

--- server/app.py
+++ server/app.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, jsonify,  make_response
 from flask_migrate import Migrate
+from ipdb import set_trace
 
 from models import db, Zookeeper, Enclosure, Animal
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app = Flask(__name__)                                       
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'  #set up to point to our existing database of app.db
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False        #set to false to avoid buliding up too much unhelpful data in memory while app is running
 
-migrate = Migrate(app, db)
+migrate = Migrate(app, db)          #this creates a Migrate instance that configures thte app and modesl for Flask-Migrate
 
-db.init_app(app)
+db.init_app(app)                    #connects our database to our application before it runs
 
 @app.route('/')
 def home():
@@ -19,15 +20,49 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.filter(Animal.id == id).first()
+    # set_trace()
+    return f'''
+    <ul>ID: {animal.id}</ul>
+    <ul>Name: {animal.name}</ul>
+    <ul>Species: {animal.species}</ul>
+    <ul>Zookeeper: {animal.zookeeper.name}</ul>
+    <ul>Enclosure: {animal.enclosure.environment}</ul>
+
+    '''
+    
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.filter(Zookeeper.id == id).first()
+    animals = zookeeper.animals
+    list_of_animals = f''
+    for animal in animals:
+       list_of_animals += f'<ul>Animal: {animal.name}</ul>\n'
+    
+    response = f'''
+        <ul>ID: {zookeeper.id}</ul>
+        <ul>Name: {zookeeper.name}</ul>
+        <ul>Birthday: {zookeeper.birthday}</ul>
+        {list_of_animals}
+        '''
+    return response
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    enclosure = Enclosure.query.filter(Enclosure.id == id).first()
+    animals = enclosure.animals
+    list_of_animals = f''
+    for animal in animals:
+       list_of_animals += f'<ul>Animal: {animal.name}</ul>\n'
+    
+    response = f'''
+        <ul>ID: {enclosure.id}</ul>
+        <ul>Environment: {enclosure.environment}</ul>
+        <ul>Open to Visitors: {enclosure.open_to_visitors}</ul>
+        {list_of_animals}
+        '''
+    return response
 
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -11,13 +11,35 @@ class Zookeeper(db.Model):
     __tablename__ = 'zookeepers'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    birthday = db.Column(db.DateTime)
+    #*Relationship
+    animals = db.relationship('Animal', back_populates='zookeeper')
+    
+    def __repr__(self):
+        return f'<Zookeeper: {self.name}'
+    
 
 class Enclosure(db.Model):
     __tablename__ = 'enclosures'
 
     id = db.Column(db.Integer, primary_key=True)
+    environment = db.Column(db.String)
+    open_to_visitors = db.Column(db.Boolean, unique=False, default=True)
+    animals = db.relationship('Animal', back_populates='enclosure')
 
 class Animal(db.Model):
     __tablename__ = 'animals'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    species = db.Column(db.String)
+    zookeeper_id = db.Column(db.Integer, db.ForeignKey('zookeepers.id'))
+    enclosure_id = db.Column(db.Integer, db.ForeignKey('enclosures.id'))
+    #!Relationships
+    zookeeper = db.relationship('Zookeeper', back_populates='animals')
+    enclosure = db.relationship('Enclosure', back_populates='animals')
+    
+    
+    def __repr__(self):
+        return f'<Animal: {self.name} | Species: {self.species}'
